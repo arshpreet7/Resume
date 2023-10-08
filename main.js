f0fbc04fd525767f2680cf4f991274797c1d70eb
arshpreet7 <89577932+arshpreet7@users.noreply.github.com>
2023-10-08 17:38:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+// function for popup1 //
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+// function for popup2 //
+function myFunction2() {
+  var popup = document.getElementById("myPopup1");
+  popup.classList.toggle("show");
+}
+// function for opening link for linkden //
+function myFunction1() {
+  window.open("https://www.linkedin.com/in/amarpreet-singh-14970386?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app");
+}
